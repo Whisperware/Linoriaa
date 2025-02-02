941c9852045ec4dc4e667e4d419206b232ea938e
Whisperware <decoys271@gmail.com>
2025-02-03 00:16:53 +0800

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -30,10 +30,10 @@ local Library = {
     HudRegistry = {};
 
     FontColor = Color3.fromRGB(255, 255, 255);
-    MainColor = Color3.fromRGB(28, 28, 28);
-    BackgroundColor = Color3.fromRGB(20, 20, 20);
-    AccentColor = Color3.fromRGB(0, 85, 255);
-    OutlineColor = Color3.fromRGB(50, 50, 50);
+    MainColor = Color3.fromRGB(29, 23, 66);
+    BackgroundColor = Color3.fromRGB(25, 29, 53);
+    AccentColor = Color3.fromRGB(127, 16, 166);
+    OutlineColor = Color3.fromRGB(60, 53, 93);
     RiskColor = Color3.fromRGB(255, 50, 50),
 
     Black = Color3.new(0, 0, 0);
